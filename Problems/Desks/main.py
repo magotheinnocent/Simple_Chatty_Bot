1d5f685acfed5e9136b1cb839f985e66004d097a
magotheinnocent <innocentp576@gmail.com>
2020-12-04 21:50:38 +0300

    Initial commit

---
+++ Problems/Desks/main.py
@@ -0,0 +1,10 @@
+# put your python code here
+import math
+
+class_one = int(input())
+class_one = math.ceil(class_one / 2)
+class_two = int(input())
+class_two = math.ceil(class_two / 2)
+class_three = int(input())
+class_three = math.ceil(class_three / 2)
+print(class_one + class_two + class_three)
