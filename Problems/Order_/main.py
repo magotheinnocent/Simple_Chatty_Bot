df59f34c1e109d52dbc3c8106a0f8dcdd914081d
magotheinnocent <innocentp576@gmail.com>
2020-12-07 19:30:44 +0300

    Initial commit

--- Problems/Order_/main.py
+++ Problems/Order_/main.py
@@ -2,7 +2,7 @@ number1 = int(input())
 number2 = int(input())
 number3 = int(input())
 
-if (number1 < number2) and (number2 <number3):
+if (number1 < number2 < number3):
     print(True)
 
 else:
---
+++ Problems/Underworld/main.py
@@ -0,0 +1,1 @@
+# the variable `coin` is already defined
