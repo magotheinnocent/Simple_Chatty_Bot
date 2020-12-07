ee5a43387878c2467f01847d144e9caf06c04a6e
magotheinnocent <innocentp576@gmail.com>
2020-12-07 19:19:32 +0300

    Initial commit

---
+++ Problems/Guessing game/main.py
@@ -0,0 +1,10 @@
+guess1 = int(input())
+guess2 = int(input())
+set_number = 6557
+
+result = guess1 * guess2
+if result == set_number:
+    print(True)
+
+else:
+    print(False)
---
+++ Problems/Order_/main.py
@@ -0,0 +1,9 @@
+number1 = int(input())
+number2 = int(input())
+number3 = int(input())
+
+if (number1 < number2) and (number2 <number3):
+    print(True)
+
+else:
+    print(False)
