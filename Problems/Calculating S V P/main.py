aeb280f23dda899247e6786f51c1045754ff66d7
magotheinnocent <innocentp576@gmail.com>
2020-12-07 11:15:28 +0300

    Initial commit

--- Problems/Calculating S V P/main.py
+++ Problems/Calculating S V P/main.py
@@ -1,1 +1,14 @@
 # put your python code here
+length = int(input())
+width = int(input())
+height = int(input())
+
+sum = (4 * (length + width + height))
+surface_area = (2 * ((length * width)
+                + (width * height)
+                + (length * height)))
+volume = (length * width * height)
+
+print(sum)
+print(surface_area)
+print(volume)
