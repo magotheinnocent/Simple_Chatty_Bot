30895fb2d896854ef08471c47502698df3d017a2
magotheinnocent <innocentp576@gmail.com>
2020-12-15 12:44:40 +0300

    Initial commit

--- Problems/List from string/main.py
+++ Problems/List from string/main.py
@@ -1,4 +1,5 @@
 # the following line reads the string to work with from the input, do not modify it, please
-input_str = input()
+input_str = list(str(input()))
 
 # your code here
+print(input_str)
